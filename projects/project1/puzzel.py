11ca739602c6cee2ca4a33cb22f6815e03bceef0
DonaldDuckEde <olivier.hartsuiker@gmail.com>
2023-04-03 14:03:51 +0200

    upload

---
+++ projects/project1/puzzel.py
@@ -0,0 +1,55 @@
+import random
+
+print("Welkom bij steen, papier, schaar")
+
+def puntGelijk():
+    print("Gelijk spel.")
+    gelijk += 1
+    stand()
+
+def puntVerloren():
+    print("De computer wint.")
+    verloren += 1
+    stand()
+
+def puntGewonnen():
+    print("Jij wint!")
+    gewonnen += 1
+    stand()
+
+def stand():
+    print(f"Gewonnen: {gewonnen}, Verloren: {verloren}, Gelijk spel: {gelijk}")
+
+mogelijkeAntwoorden = ["steen", "papier", "schaar"]
+gewonnen, verloren, gelijk = 0, 0, 0
+
+while True:
+    antwoord = input("Steen, papier of schaar? ")
+    antwoord = antwoord.lower()
+
+    if antwoord != "steen" and antwoord != "papier" and antwoord != "schaar":
+        print("Antwoord: steen of papier of schaar.")
+        break
+    else:
+        computerAntwoord = random.choice(mogelijkeAntwoorden)
+        
+        print(f"{antwoord} VS {computerAntwoord}")
+
+        if computerAntwoord == antwoord:
+            puntGelijk()
+        elif computerAntwoord == "schaar" and antwoord == "papier":
+            stand()
+        elif computerAntwoord == "papier" and antwoord == "steen":
+            stand()
+        elif computerAntwoord == "steen" and antwoord == "schaar":
+            stand()
+        elif antwoord == "papier" and computerAntwoord == "steen":
+            puntGewonnen()
+        elif antwoord == "schaar" and computerAntwoord == "papier":
+            puntGewonnen()
+        elif antwoord == "steen" and  computerAntwoord == "schaar":
+            puntGewonnen()
+        elif antwoord == "sluiten":
+            False
+        else:
+            print("Er is denk ik iets fout gegaan.")
