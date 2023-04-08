aaff00ed85c33b06493762cf8fc75a101f8acb69
DonaldDuckEde <olivier.hartsuiker@gmail.com>
2023-04-08 18:47:23 +0200

    update

--- projects/python-temp/temp1.py
+++ projects/python-temp/temp1.py
@@ -6,11 +6,12 @@ location = "8/ "
 while i == 8:
     inputCommand = input(f"{location.strip(' ')}:~$ ")
     
-    # strips the input command so it can be used in the rest of the program
+    # copies the input command so it can be used in the rest of the program
     tempCommand = copy.deepcopy(inputCommand)
     tempCommand1 = copy.deepcopy(inputCommand)
     tempCommand = tempCommand.strip()
     
+    # splits the input command into the main command and the sub command, so it can be read with the simple if else statement
     mainCommand = tempCommand.split()[0]
     subCommand = tempCommand1.split()[1]
     
