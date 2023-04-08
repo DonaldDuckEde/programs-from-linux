46b627e9d139285e51451d84f01752dacf39e3f5
DonaldDuckEde <olivier.hartsuiker@gmail.com>
2023-04-08 12:12:51 +0200

    changes

--- projects/python-temp/temp1.py
+++ projects/python-temp/temp1.py
@@ -7,12 +7,13 @@ while i == 8:
     inputCommand = input(f"{location.strip(' ')}:~$ ")
     
     tempCommand = copy.deepcopy(inputCommand)
+    tempCommand1 = copy.deepcopy(inputCommand)
     tempCommand = tempCommand.strip()
     
     mainCommand = tempCommand.split()[0]
-    subCommand = tempCommand.split()[1]
+    subCommand = tempCommand1.split()[1]
     
     if mainCommand == "cd":
-        
+        print(f"{location.strip(' ')}:~$ cd {subCommand}")
     elif mainCommand == "exit":
         break
