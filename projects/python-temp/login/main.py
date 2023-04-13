e61629620aafddb35cc64d27564df0aa93468640
DonaldDuckEde <olivier.hartsuiker@gmail.com>
2023-04-13 22:44:26 +0200

    changes

--- projects/python-temp/login/main.py
+++ projects/python-temp/login/main.py
@@ -1,6 +1,16 @@
 path = "o/,project/,"
+sudoPower = False
+
 
 while True:
     command = input(f"{path}$ ")
     if command == "sudo":
         inputSudoPass = input("sudo: ")
+        if inputSudoPass == sudoPass:
+            sudoPower = True
+        else:
+            print("Please enter a valid sudo key: ")
+    elif command == "keys":
+        print("welcome to the key environment.")
+        changeKey = input("Key to change:")
+        
