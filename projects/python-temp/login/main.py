3144637c730310811fb94ec4388e45f149462faf
DonaldDuckEde <olivier.hartsuiker@gmail.com>
2023-04-14 21:58:59 +0200

    added path feature

--- projects/python-temp/login/main.py
+++ projects/python-temp/login/main.py
@@ -14,17 +14,17 @@ class VirtualPath:
 
 my_path = VirtualPath('root')
 
-new_folder = input('Enter a new folder name: ')
-my_path = my_path.join(new_folder)
-
 def strikes(strikes):
     warnStrikes += 1
     if warnStrikes == 3:
         print("You have to much false uses please restart the evenirement.")
         exit(0)
 
+def enter():
+    print()
+
 while True:
-    command = input(f"{path}$ ")
+    command = input(f"{my_path}$ ")
     if command == "sudo":
         inputSudoPass = input("sudo: ")
         if inputSudoPass == sudoPass:
@@ -32,8 +32,9 @@ while True:
         else:
             print("Please enter a valid sudo key: ")
     elif command == "keys":
+        enter()
         print("welcome to the key environment.")
-        changeKey = input("Key to change:")
+        changeKey = input("Key to change: ")
         
         if changeKey == "sudo":
             newSudoPass = input("Sudo key: ")
@@ -41,5 +42,6 @@ while True:
                 newSudoPass = sudoPass
             else:
                 strikes(warnStrikes)
-    elif command == "cd":
-        
+    elif command == "mkdir":
+        new_folder = input("Enter folder name: ")
+        my_path = my_path.join(new_folder)
