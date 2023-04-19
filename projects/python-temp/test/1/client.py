dcfeb9e991aca0c69f3aee1a2c81c9aa5822e587
DonaldDuckEde <olivier.hartsuiker@gmail.com>
2023-04-19 15:59:49 +0200

    changes

---
+++ projects/python-temp/test/1/client.py
@@ -0,0 +1,13 @@
+import socket
+
+HOST = '192.168.178.210'
+PORT = 50007
+
+for i in range(999):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        commandInput = input("command: ")
+        s.sendall(commandInput.encode())
+        data = s.recv(1024)
+
+    print('Server:', repr(data.decode('utf-8')))
---
+++ projects/python-temp/test/1/server.py
@@ -0,0 +1,32 @@
+import socket
+import threading
+
+HOST = '192.168.178.210'
+PORT = 50007
+
+def handle_client(conn, addr):
+    print('Request by', addr)
+    while True:
+        rawData = conn.recv(1024)
+        if not rawData:
+            break
+        
+    data = rawData.decode('utf-8')
+    
+    if data == "quit":
+        
+    
+    conn.sendall(data)
+    conn.close()
+
+def start_server():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        while True:
+            conn, addr = s.accept()
+            t = threading.Thread(target=handle_client, args=(conn, addr))
+            t.start()
+
+if __name__ == '__main__':
+    start_server()
